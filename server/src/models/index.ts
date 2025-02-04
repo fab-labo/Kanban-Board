485cceadc11bd99d3e88f720c6dd09b430b50f7e
Fabricio Laboriel <fabriciolaboriel@gmail.com>
2025-02-03 21:11:11 -0700

    connect to postgresql

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,15 +5,20 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+let sequelize: Sequelize;
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL as string);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME as string,
+    process.env.DB_USER as string,
+    process.env.DB_PW as string,
+    {
       host: 'localhost',
       dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+    },
+  );
+}
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
